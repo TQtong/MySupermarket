67864c0c1a61e03517f77331b450ad76276ff885
TQ <tangqi@southzn.com>
2022-09-06 19:25:22 +0800

    添加项目文件。

---
+++ MySupermarket/MySupermarket/Views/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace MySupermarket.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //窗口拖动
+            colorZone.MouseMove += (s, e) =>
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                    this.DragMove();
+                }
+            };
+
+            //双击窗口放大
+            colorZone.MouseDoubleClick += (s, e) =>
+            {
+                if (this.WindowState == WindowState.Normal)
+                {
+                    this.WindowState = WindowState.Maximized;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Normal;
+                }
+            };
+        }
+
+        private void LeftOpenMenuToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            var timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(500) };
+            timer.Start();
+            timer.Tick += (sender, args) =>
+            {
+                timer.Stop();
+                if (LeftOpenMenuToggleButton.IsChecked == true)
+                {
+                    LeftOpenMenuToggleButton.Visibility = Visibility.Collapsed;
+                    LeftOpenMenuToggleButton.IsChecked = false;
+                    LeftCloseMenuToggleButton.Visibility = Visibility.Visible;
+                }
+            };
+
+
+        }
+
+        private void LeftCloseMenuToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            var timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(500) };
+            timer.Start();
+            timer.Tick += (sender, args) =>
+            {
+                timer.Stop();
+                if (LeftCloseMenuToggleButton.IsChecked == false)
+                {
+                    LeftCloseMenuToggleButton.Visibility = Visibility.Collapsed;
+                    LeftCloseMenuToggleButton.IsChecked = true;
+                    LeftOpenMenuToggleButton.Visibility = Visibility.Visible;
+                }
+            };
+
+        }
+    }
+}
---
+++ MySupermarket/Services/MySupermarket.Services.Interfaces/IMessageService.cs
@@ -0,0 +1,7 @@
+﻿namespace MySupermarket.Services.Interfaces
+{
+    public interface IMessageService
+    {
+        string GetMessage();
+    }
+}
